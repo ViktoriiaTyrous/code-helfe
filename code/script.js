d3a257f9c777960f43c846c805355eff979cb3f3
Viktoria Tyrous <viktoriatyrous@Viktorias-MacBook-Pro.local>
2023-04-28 10:47:31 +0400

    lottie

--- code/script.js
+++ code/script.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById("myBtn");
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function () {
   const dots = document.getElementById("dots");
   const moreText = document.getElementById("more");
 
@@ -32,149 +32,140 @@ closeButton.onclick = hidePopup;
 const element = document.querySelector(".js-choice");
 const choices = new Choices(element, {
   searchEnabled: false,
-  })
+});
 
 const anim = lottie;
-anim.loadAnimation ({
-    container: document.querySelector('#lottie_1'),
-    render: 'svg',
-    loop: true,
-    autoplay: true,
-    path: './animation/prana_02.json'
-})
-anim.loadAnimation ({
-    container: document.querySelector('#lottie_2'),
-    render: 'svg',
-    loop: true,
-    autoplay: true,
-    path: './animation/01.json'
-})
-anim.loadAnimation ({
-    container: document.querySelector('#lottie_6'),
-    render: 'svg',
-    loop: true,
-    autoplay: true,
-    path: './animation/Musse_002.json'
-})
-anim.loadAnimation ({
-    container: document.querySelector('#lottie_3'),
-    render: 'svg',
-    loop: true,
-    autoplay: true,
-    path: './animation/data.json'
-})
-anim.loadAnimation ({
-    container: document.querySelector('#lottie_9'),
-    render: 'svg',
-    loop: true,
-    autoplay: true,
-    path: './animation/sota_001.json'
-})
+anim.loadAnimation({
+  container: document.querySelector("#lottie_1"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./animation/prana_02.json",
+});
+anim.loadAnimation({
+  container: document.querySelector("#lottie_2"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./animation/01.json",
+});
+anim.loadAnimation({
+  container: document.querySelector("#lottie_6"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./animation/Musse_002.json",
+});
+anim.loadAnimation({
+  container: document.querySelector("#lottie_3"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./animation/old.json",
+});
+anim.loadAnimation({
+  container: document.querySelector("#lottie_9"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./animation/new.json",
+});
 
-
-
-
-    var swiper = new Swiper(".mySwiper_1", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    disabledClass: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
-      },
+var swiper = new Swiper(".mySwiper_1", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  disabledClass: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is <= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
-    breakpoints: {
-        // when window width is <= 320px
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 10
-        },
-        // when window width is <= 480px
-        480: {
-          slidesPerView: 1,
-          spaceBetween: 20
-        },
-        540: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        },
-        // when window width is <= 640px
-        800: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        }
-      }
+    // when window width is <= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is <= 640px
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+var swiper = new Swiper(".mySwiper_2", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const categorySelect = document.querySelector("#project-category");
+const contentBoxes = document.querySelectorAll(".content-box");
+
+categorySelect.addEventListener("change", () => {
+  const selectedCategory = categorySelect.value;
+
+  contentBoxes.forEach((box) => box.classList.remove("active"));
+
+  const selectedBox = document.querySelector(`#${selectedCategory}`);
+  selectedBox.classList.add("active");
+});
+
+const tabs = document.querySelectorAll(".tab-btn");
+const content = document.querySelectorAll(".content-box");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    var line = document.querySelector(".line");
+    line.style.width = e.target.offsetWidth + "px";
+    line.style.left = e.target.offsetLeft + "px";
+
+    content.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    content[index].classList.add("active");
   });
-  var swiper = new Swiper(".mySwiper_2", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    slidesPerGroup:1,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
-      },
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  
-  });
-  const categorySelect = document.querySelector('#project-category');
-  const contentBoxes = document.querySelectorAll('.content-box');
-  
-  categorySelect.addEventListener('change', () => {
-    const selectedCategory = categorySelect.value;
+});
 
-    contentBoxes.forEach(box => box.classList.remove('active'));
+document.addEventListener("click", documentClick);
 
-    const selectedBox = document.querySelector(`#${selectedCategory}`);
-    selectedBox.classList.add('active');
-  });
-  
-  
-  
-  
-  
-  
-  const tabs = document.querySelectorAll('.tab-btn');
-  const content = document.querySelectorAll('.content-box');
-
-  tabs.forEach((tab, index) => {
-    tab.addEventListener('click',(e)=>{
-        tabs.forEach(tab =>{
-            tab.classList.remove('active')
-        })
-        tab.classList.add('active');
-        var line = document.querySelector(".line");
-        line.style.width = e.target.offsetWidth + "px";
-        line.style.left = e.target.offsetLeft + "px";
-        
-        content.forEach(tab =>{
-            tab.classList.remove('active')
-        })
-       content[index].classList.add('active')
-    })
-  })
-
-  document.addEventListener("click", documentClick);
-
-function documentClick(e){
-    const targetItem = e.target;
-    if (targetItem.closest('.burger')) {
-        document.body.classList.toggle('burger-open');
-    }
+function documentClick(e) {
+  const targetItem = e.target;
+  if (targetItem.closest(".burger")) {
+    document.body.classList.toggle("burger-open");
+  }
 }
 // function validation(form) {
 //   const allInputs = form.querySelectorAll('input');
@@ -195,108 +186,102 @@ function documentClick(e){
 
 // })
 
-const name = document.getElementById('name');
-const phone = document.getElementById('phone');
-const email = document.getElementById('email');
+const name = document.getElementById("name");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
 
 // Get the error message containers
-const nameError = document.querySelector('.name-error');
-const phoneError = document.querySelector('.phone-error');
-const emailError = document.querySelector('.email-error');
+const nameError = document.querySelector(".name-error");
+const phoneError = document.querySelector(".phone-error");
+const emailError = document.querySelector(".email-error");
 
 // Set up a regular expression pattern for phone numbers
 const phonePattern = /^\+?\d{1,3}[\s.-]?\d{2,4}[\s.-]?\d{3,4}[\s.-]?\d{3,4}$/;
 
 // Reset error messages
-nameError.textContent = '';
-phoneError.textContent = '';
-emailError.textContent = '';
+nameError.textContent = "";
+phoneError.textContent = "";
+emailError.textContent = "";
 
-let formButton = document.querySelector('.button-form');
+let formButton = document.querySelector(".button-form");
 
 function validateForm() {
   // Get the input fields
 
-
   // Check if the name field is empty
-  if (name.value.trim() === '') {
-    nameError.textContent = 'Bitte geben Sie Ihren Namen ein';
+  if (name.value.trim() === "") {
+    nameError.textContent = "Bitte geben Sie Ihren Namen ein";
     name.classList.add("error-input");
     formButton.disabled = true;
     formButton.classList.add("disabled");
     return false;
-
   } else {
-    nameError.textContent = '';
+    nameError.textContent = "";
     name.classList.remove("error-input");
-    checkFormValidity()
+    checkFormValidity();
   }
 
   // Check if the phone field is empty
-  if (phone.value.trim() === '') {
-    phoneError.textContent = 'Bitte geben sie ihre Telefonnummer ein';
+  if (phone.value.trim() === "") {
+    phoneError.textContent = "Bitte geben sie ihre Telefonnummer ein";
     phone.classList.add("error-input");
     formButton.disabled = true;
     formButton.classList.add("disabled");
     return false;
   } else {
-    phoneError.textContent = '';
+    phoneError.textContent = "";
     phone.classList.remove("error-input");
-    checkFormValidity()
+    checkFormValidity();
   }
 
   // Check if the phone number matches the pattern
   if (!phonePattern.test(phone.value.trim())) {
-    phoneError.textContent = 'Bitte geben Sie eine gültige E-Mail-Adresse ein';
+    phoneError.textContent = "Bitte geben Sie eine gültige E-Mail-Adresse ein";
     formButton.disabled = true;
     formButton.classList.add("disabled");
     return false;
   } else {
-    phoneError.textContent = '';
+    phoneError.textContent = "";
     phone.classList.remove("error-input");
-    checkFormValidity()
+    checkFormValidity();
   }
 
   // Check if the email field is empty
-  if (email.value.trim() === '') {
-    emailError.textContent = 'Geben Sie bitte Ihre Email-Adresse ein';
+  if (email.value.trim() === "") {
+    emailError.textContent = "Geben Sie bitte Ihre Email-Adresse ein";
     formButton.disabled = true;
     formButton.classList.add("disabled");
     return false;
   } else {
-    emailError.textContent = '';
+    emailError.textContent = "";
     email.classList.remove("error-input");
-    checkFormValidity()
+    checkFormValidity();
   }
 
   // Check if the email address is valid
   if (!isValidEmail(email.value.trim())) {
-    emailError.textContent = 'Bitte geben Sie eine gültige E-Mail-Adresse ein';
+    emailError.textContent = "Bitte geben Sie eine gültige E-Mail-Adresse ein";
     formButton.disabled = true;
     formButton.classList.add("disabled");
     return false;
   } else {
-    emailError.textContent = '';
+    emailError.textContent = "";
     email.classList.remove("error-input");
-    checkFormValidity()
+    checkFormValidity();
   }
- 
+
   // If all the fields are valid, submit the form
   formButton.disabled = false;
   formButton.classList.remove("disabled");
 
-
   return true;
-
 }
-
 
 // Add event listeners to the input fields
 
-
-name.addEventListener('input', validateForm);
-phone.addEventListener('input', validateForm);
-email.addEventListener('input', validateForm);
+name.addEventListener("input", validateForm);
+phone.addEventListener("input", validateForm);
+email.addEventListener("input", validateForm);
 
 function checkFormValidity() {
   if (name.validity.valid && phone.validity.valid && email.validity.valid) {
@@ -320,7 +305,6 @@ function checkFormValidity() {
 
 // function validateFormPop() {
 //   // Get the input fields
-
 
 //   // Check if the name field is empty
 //   if (namePop.value.trim() === '') {
@@ -384,19 +368,16 @@ function checkFormValidity() {
 //     emailPop.classList.remove("error-input-pop");
 //     checkFormValidityPop()
 //   }
- 
+
 //   // If all the fields are valid, submit the form
 //   formButtonPop.disabled = false;
 //   formButtonPop.classList.remove("disabled");
-
 
 //   return true;
 
 // }
 
-
 // // Add event listeners to the input fields
-
 
 // namePop.addEventListener('input', validateFormPop);
 // phonePop.addEventListener('input', validateFormPop);
@@ -411,75 +392,76 @@ function checkFormValidity() {
 
 function validateFormPop() {
   // Get the input fields
-  const namePop = document.querySelector('#namePop');
-  const phonePop = document.querySelector('#phonePop');
-  const emailPop = document.querySelector('#emailPop');
-  const formButtonPop = document.querySelector('.button-form-pop');
-  const nameErrorPop = document.querySelector('.name-error-pop');
-  const phoneErrorPop = document.querySelector('.phone-error-pop');
-  const emailErrorPop = document.querySelector('.email-error-pop');
+  const namePop = document.querySelector("#namePop");
+  const phonePop = document.querySelector("#phonePop");
+  const emailPop = document.querySelector("#emailPop");
+  const formButtonPop = document.querySelector(".button-form-pop");
+  const nameErrorPop = document.querySelector(".name-error-pop");
+  const phoneErrorPop = document.querySelector(".phone-error-pop");
+  const emailErrorPop = document.querySelector(".email-error-pop");
 
   // Check if the name field is empty
-  if (namePop.value.trim() === '') {
-    nameErrorPop.textContent = 'Bitte geben Sie Ihren Namen ein';
+  if (namePop.value.trim() === "") {
+    nameErrorPop.textContent = "Bitte geben Sie Ihren Namen ein";
     namePop.classList.add("error-input-pop");
     formButtonPop.disabled = true;
     formButtonPop.classList.add("disabled");
     return false;
-
   } else {
-    nameErrorPop.textContent = '';
+    nameErrorPop.textContent = "";
     namePop.classList.remove("error-input");
-    checkFormValidityPop()
+    checkFormValidityPop();
   }
 
   // Check if the phone field is empty
-  if (phonePop.value.trim() === '') {
-    phoneErrorPop.textContent = 'Bitte geben sie ihre Telefonnummer ein';
+  if (phonePop.value.trim() === "") {
+    phoneErrorPop.textContent = "Bitte geben sie ihre Telefonnummer ein";
     phonePop.classList.add("error-input-pop");
     formButtonPop.disabled = true;
     formButtonPop.classList.add("disabled");
     return false;
   } else {
-    phoneErrorPop.textContent = '';
+    phoneErrorPop.textContent = "";
     phonePop.classList.remove("error-input-pop");
-    checkFormValidityPop()
+    checkFormValidityPop();
   }
 
   // Check if the phone number matches the pattern
   if (!phonePattern.test(phonePop.value.trim())) {
-    phoneErrorPop.textContent = 'Bitte geben Sie eine gültige Telefonnummer ein';
+    phoneErrorPop.textContent =
+      "Bitte geben Sie eine gültige Telefonnummer ein";
     formButtonPop.disabled = true;
     formButtonPop.classList.add("disabled");
     return false;
   } else {
-    phoneErrorPop.textContent = '';
+    phoneErrorPop.textContent = "";
     phonePop.classList.remove("error-input-pop");
-    checkFormValidityPop()
+    checkFormValidityPop();
   }
 
   // Check if the email field is empty
-  if (emailPop.value.trim() === '') {
-    emailErrorPop.textContent = 'Geben Sie bitte Ihre Email-Adresse ein';
+  if (emailPop.value.trim() === "") {
+    emailErrorPop.textContent = "Geben Sie bitte Ihre Email-Adresse ein";
     formButtonPop.disabled = true;
     formButtonPop.classList.add("disabled");
     return false;
   } else {
-    emailErrorPop.textContent = '';
+    emailErrorPop.textContent = "";
     emailPop.classList.remove("error-input-pop");
-    checkFormValidityPop()
+    checkFormValidityPop();
   }
 
   // Check if the email address is valid
   if (!isValidEmailPop(emailPop.value.trim())) {
-    emailErrorPop.textContent = 'Bitte geben Sie eine gültige E-Mail-Adresse ein';
+    emailErrorPop.textContent =
+      "Bitte geben Sie eine gültige E-Mail-Adresse ein";
     formButtonPop.disabled = true;
     formButtonPop.classList.add("disabled");
     return false;
   } else {
-    emailErrorPop.textContent = '';
+    emailErrorPop.textContent = "";
     emailPop.classList.remove("error-input-pop");
-    checkFormValidityPop()
+    checkFormValidityPop();
   }
 
   // If all the fields are valid, submit the form
@@ -490,17 +472,22 @@ function validateFormPop() {
 }
 
 // Add event listeners to the input fields
-const namePop = document.querySelector('#namePop');
-const phonePop = document.querySelector('#phonePop');
-const emailPop = document.querySelector('#emailPop');
-const formButtonPop = document.querySelector('.button-form-pop');
+const namePop = document.querySelector("#namePop");
+const phonePop = document.querySelector("#phonePop");
+const emailPop = document.querySelector("#emailPop");
+const formButtonPop = document.querySelector(".button-form-pop");
 
-namePop.addEventListener('input', validateFormPop);
-phonePop.addEventListener('input', validateFormPop);
-emailPop.addEventListener('input', validateFormPop);
+namePop.addEventListener("input", validateFormPop);
+phonePop.addEventListener("input", validateFormPop);
+emailPop.addEventListener("input", validateFormPop);
 
 function checkFormValidityPop() {
-  if (namePop.validity.valid && phonePop.validity.valid && emailPop.validity.valid) {
+  if (
+    namePop.validity.valid &&
+    phonePop.validity.valid &&
+    emailPop.validity.valid
+  ) {
     formButtonPop.disabled = false;
-formButtonPop.classList.remove("disabled")  }
+    formButtonPop.classList.remove("disabled");
+  }
 }
