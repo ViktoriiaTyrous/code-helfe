3b37e13f92e3b48a7cc0cba1b2e0c24a88174ac7
Viktoria Tyrous <viktoriatyrous@Viktorias-MacBook-Pro.local>
2023-05-02 14:51:22 +0200

    design page

--- code/script.js
+++ code/script.js
@@ -184,3 +184,41 @@ btn.addEventListener("click", function () {
     moreText.style.display = "inline";
   }
 });
+
+const tabButtons = document.querySelectorAll('.structure__btn');
+const tabContentBoxes = document.querySelectorAll('.structure__content-box');
+
+const arrowRightBtn = document.querySelector('.arrow-right');
+const arrowLeftBtn = document.querySelector('.arrow-left');
+
+let currentTabIndex = 0;
+
+function showTab(index) {
+  tabContentBoxes.forEach(box => box.classList.remove('active'));
+  tabContentBoxes[index].classList.add('active');
+  tabButtons.forEach(button => button.classList.remove('active'));
+  tabButtons[index].classList.add('active');
+  currentTabIndex = index;
+}
+
+tabButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    showTab(index);
+  });
+});
+
+arrowRightBtn.addEventListener('click', () => {
+  currentTabIndex++;
+  if (currentTabIndex >= tabContentBoxes.length) {
+    currentTabIndex = 0;
+  }
+  showTab(currentTabIndex);
+});
+
+arrowLeftBtn.addEventListener('click', () => {
+  currentTabIndex--;
+  if (currentTabIndex < 0) {
+    currentTabIndex = tabContentBoxes.length - 1;
+  }
+  showTab(currentTabIndex);
+});
