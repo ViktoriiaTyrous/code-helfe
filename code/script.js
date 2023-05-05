0e36d5c3793a98ac6c8def59719f45dc6dc35073
Viktoria Tyrous <viktoriatyrous@Viktorias-MacBook-Pro.local>
2023-05-05 14:57:06 +0200

    text for service personalisation

--- code/script.js
+++ code/script.js
@@ -130,6 +130,7 @@ var swiper = new Swiper(".mySwiper_1", {
     },
   },
 });
+
 var swiper = new Swiper(".mySwiper_2", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -205,61 +206,14 @@ btn.addEventListener("click", function () {
   }
 });
 
-const tabButtons = document.querySelectorAll('.structure__btn');
-const tabContentBoxes = document.querySelectorAll('.structure__content-box');
 
-const arrowRightBtn = document.querySelector('.arrow-right');
-const arrowLeftBtn = document.querySelector('.arrow-left');
 
-let currentTabIndex = 0;
-
-function showTab(index) {
-  tabContentBoxes.forEach(box => box.classList.remove('active'));
-  tabContentBoxes[index].classList.add('active');
-  tabButtons.forEach(button => button.classList.remove('active'));
-  tabButtons[index].classList.add('active');
-  currentTabIndex = index;
-}
-
-tabButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    showTab(index);
-  });
-});
-
-arrowLeftBtn.addEventListener('click', () => {
-  currentTabIndex++;
-  if (currentTabIndex >= tabContentBoxes.length) {
-    currentTabIndex = 0;
+let personalBtn = document.querySelector('.personal-btn');
+personalBtn.addEventListener('click', function() {
+  let textForPersonal = document.querySelector('.text-for-personal');
+  if (textForPersonal.classList.contains('active')) {
+    textForPersonal.classList.remove('active');
+  } else {
+    textForPersonal.classList.add('active');
   }
-  showTab(currentTabIndex);
 });
-
-arrowRightBtn.addEventListener('click', () => {
-  currentTabIndex--;
-  if (currentTabIndex < 0) {
-    currentTabIndex = tabContentBoxes.length - 1;
-  }
-  showTab(currentTabIndex);
-});
-
-const btns = document.querySelectorAll('.structure__btn-mobile');
-const contentBox = document.querySelectorAll('.structure__content-box-mobile');
-
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', () => {
-    if (contentBox[i].style.display === 'block') {
-      contentBox[i].style.display = 'none';
-      btns[i].classList.remove('active');
-    } else {
-      contentBox.forEach(box => {
-        box.style.display = 'none';
-      });
-      contentBox[i].style.display = 'block';
-      btns.forEach(btn => {
-        btn.classList.remove('active');
-      });
-      btns[i].classList.add('active');
-    }
-  });
-}
---
+++ code/structure_sctipt.js
@@ -0,0 +1,58 @@
+const tabButtons = document.querySelectorAll('.structure__btn');
+const tabContentBoxes = document.querySelectorAll('.structure__content-box');
+
+const arrowRightBtn = document.querySelector('.arrow-right');
+const arrowLeftBtn = document.querySelector('.arrow-left');
+
+let currentTabIndex = 0;
+
+function showTab(index) {
+  tabContentBoxes.forEach(box => box.classList.remove('active'));
+  tabContentBoxes[index].classList.add('active');
+  tabButtons.forEach(button => button.classList.remove('active'));
+  tabButtons[index].classList.add('active');
+  currentTabIndex = index;
+}
+
+tabButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    showTab(index);
+  });
+});
+
+arrowLeftBtn.addEventListener('click', () => {
+  currentTabIndex++;
+  if (currentTabIndex >= tabContentBoxes.length) {
+    currentTabIndex = 0;
+  }
+  showTab(currentTabIndex);
+});
+
+arrowRightBtn.addEventListener('click', () => {
+  currentTabIndex--;
+  if (currentTabIndex < 0) {
+    currentTabIndex = tabContentBoxes.length - 1;
+  }
+  showTab(currentTabIndex);
+});
+
+const btns = document.querySelectorAll('.structure__btn-mobile');
+const contentBox = document.querySelectorAll('.structure__content-box-mobile');
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', () => {
+    if (contentBox[i].style.display === 'block') {
+      contentBox[i].style.display = 'none';
+      btns[i].classList.remove('active');
+    } else {
+      contentBox.forEach(box => {
+        box.style.display = 'none';
+      });
+      contentBox[i].style.display = 'block';
+      btns.forEach(btn => {
+        btn.classList.remove('active');
+      });
+      btns[i].classList.add('active');
+    }
+  });
+}
