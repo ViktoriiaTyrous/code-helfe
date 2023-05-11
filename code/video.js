f95ae7857de1925386875a384fc77fe88a5ec9a9
Viktoria Tyrous <vikatyrous144@gmail.com>
2023-05-12 01:32:06 +0200

    Impressum page

--- code/video.js
+++ code/video.js
@@ -1,18 +1,20 @@
-let videoDesign = document.getElementById("myVideo");
-let videoButtonDesign = document.getElementById("video-button");
-let videoTextDesign = document.getElementById("videoBtn-text");
+let video = document.getElementById("myVideo");
+let videoButton = document.getElementById("video-button");
+let videoText = document.getElementById("videoBtn-text");
 
-videoButtonDesign.addEventListener("click", function() {
-  if (videoDesign.muted) {
-    videoDesign.play();
-    videoDesign.muted = false;
-    videoButtonDesign.querySelector('.buttons__btn-video__icon').src = "./images/video-pause.svg";
+videoButton.addEventListener("click", function() {
+  if (video.muted) {
+    video.play();
+    video.muted = false;
+    videoButton.querySelector('.buttons__btn-video__icon').src = "./images/video-pause.svg";
     videoText.innerHTML = "Videos stoppen";
 
   } else {
-    videoDesign.pause();
-    videoDesign.muted = true;
-    videoButtonDesign.querySelector('.buttons__btn-video__icon').src = "./images/video.svg";
+    video.pause();
+    video.muted = true;
+    videoButton.querySelector('.buttons__btn-video__icon').src = "./images/video.svg";
     videoText.innerHTML = "Video abspielen";
+    
+    
   }
 });
--- design/video-design.js
+++ design/video-design.js
@@ -6,13 +6,17 @@ videoButton.addEventListener("click", function() {
   if (video.muted) {
     video.play();
     video.muted = false;
-    video.classList.add("play")
+  
     videoButton.querySelector('.buttons__btn-video__icon').src = "../images/video-pause.svg";
-    videoTextDesign.innerHTML = "Videos stoppen";
-  } else {
-    video.classList.remove("play")
+    videoText.innerHTML = "Videos stoppen";
+  } 
+  else {
+    video.pause();
+    video.muted = true;
+    videoButton.querySelector('.buttons__btn-video__icon').src = "../images/video.svg";
+    videoText.innerHTML = "Video abspielen";
   }
- 
+  
 });
 
 
