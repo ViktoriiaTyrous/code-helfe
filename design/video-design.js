32bb3cab7668254c8fe5985ff24cef9e9712d1c8
Viktoria Tyrous <vikatyrous144@gmail.com>
2023-05-12 02:30:33 +0200

    video

--- design/video-design.js
+++ design/video-design.js
@@ -7,13 +7,14 @@ videoButton.addEventListener('click', function () {
   if (video.paused) {
       video.play();
       videoText.innerHTML = "Video abspielen";
+      videoButton.querySelector('.buttons__btn-video__icon').src = "../images/video.svg";
     
   } else if (video.muted) {
     video.play();
     video.muted = false;
     videoText.innerHTML = "Video abspielen";
     videoButton.querySelector('.buttons__btn-video__icon').src = "../images/video.svg";
-  } else
+  } else 
   video.pause();
   videoButton.querySelector('.buttons__btn-video__icon').src = "../images/video-pause.svg";
   videoText.innerHTML = "Videos stoppen";
