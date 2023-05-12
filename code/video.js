9b0aa2be05b03dac51f82ad6e0998730ae7cf6ce
Viktoria Tyrous <vikatyrous144@gmail.com>
2023-05-12 04:25:29 +0200

    Impressum page

--- code/video.js
+++ code/video.js
@@ -5,9 +5,13 @@ let videoText = document.getElementById("videoBtn-text");
 videoButton.addEventListener('click', function () {
   if (video.paused) {
     video.play();
-    video.style.display = "block"
+    if (window.innerWidth < 600){
+      video.style.display = "none";
+    } else {
+      video.style.display = "block";
     videoText.innerHTML = "Video abspielen";
     videoButton.querySelector('.buttons__btn-video__icon').src = "./images/video.svg";  
+  }
   } else if (video.muted) {
     video.pause();
     video.muted = true;
