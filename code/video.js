191e6f41d256652a78c029d7bff8654cdcf9c13a
Viktoria Tyrous <vikatyrous144@gmail.com>
2023-05-12 04:23:28 +0200

    video

--- code/video.js
+++ code/video.js
@@ -2,24 +2,28 @@ let video = document.getElementById("myVideo");
 let videoButton = document.getElementById("video-button");
 let videoText = document.getElementById("videoBtn-text");
 
-
 videoButton.addEventListener('click', function () {
   if (video.paused) {
-      video.play();
-      video.style.display = "block"
-      videoText.innerHTML = "Video abspielen";
-      videoButton.querySelector('.buttons__btn-video__icon').src = "./images/video.svg";  
+    video.play();
+    video.style.display = "block"
+    videoText.innerHTML = "Video abspielen";
+    videoButton.querySelector('.buttons__btn-video__icon').src = "./images/video.svg";  
   } else if (video.muted) {
     video.pause();
     video.muted = true;
-    video.style.display = "block";
-    videoButton.querySelector('.buttons__btn-video__icon').src = "./images/video-pause.svg";
-    videoText.innerHTML = "Videos stoppen";
-  } else 
-  video.play();
-  video.style.display = "none"
-  video.muted = false;
-  videoText.innerHTML = "Video abspielen";
-  videoButton.querySelector('.buttons__btn-video__icon').src = "./images/video.svg";
+    if (window.innerWidth < 600){
+      video.style.display = "none";
+    } else {
+      video.style.display = "block";
+      videoButton.querySelector('.buttons__btn-video__icon').src = "./images/video-pause.svg";
+      videoText.innerHTML = "Videos stoppen";
+    }
+   
+  } else {
+    video.pause();
+    video.muted = false;
+    videoText.innerHTML = "Video abspielen";
+    videoButton.querySelector('.buttons__btn-video__icon').src = "./images/video.svg";
+  }
 });
 
