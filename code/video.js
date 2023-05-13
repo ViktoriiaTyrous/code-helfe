1f92d1096f486ccb30dcc3cba4ae7fbe2cdfcbdf
Viktoria Tyrous <vikatyrous144@gmail.com>
2023-05-13 05:44:09 +0200

    video

--- code/video.js
+++ code/video.js
@@ -1,4 +1,4 @@
-let video = document.getElementById("video");
+let video = document.getElementById("myVideo");
 let videoButton = document.getElementById("video-button");
 let videoText = document.getElementById("videoBtn-text");
 
